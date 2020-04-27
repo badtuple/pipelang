68fc7103c4fdbc6598b8ba313876539b6094c579
badtuple <the.legend@badtuple.me>
2020-04-27 00:40:27 -0700

    make filter a trait and not a boxed function

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,35 @@
 pub struct Interpreter;
 
 impl Interpreter {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {}
     }
 
-    fn exec(&self, query: String) {
+    pub fn exec(&self, query: String) {
         // TODO: Parse query into AST
 
         // TODO: AST to runnable Pipeline
-        let pipeline = Pipeline {
-            data: vec![Datum { data: 4 }],
-            filters: vec![],
-        };
 
+        // Execute the pipeline on the data
+        let pipeline = Pipeline::new(vec![Datum { data: 4 }], vec![]);
         self.exec_pipeline(pipeline);
     }
 
-    fn exec_pipeline(&self, pipeline: Pipeline) -> Vec<Box<Option<Datum>>> {
+    fn exec_pipeline(&self, pipeline: Pipeline) -> Vec<Option<Datum>> {
         // TODO: Right now we're collecting all the values upfront, which isn't the best call for
         // long running pipelines. Eventually we'll need to pass results up to the user as they
         // finish running through the pipeline;
 
-        let filters = pipeline.filters.clone();
-
         pipeline
             .data
-            .into_iter()
+            .iter()
             .map(|datum| {
                 // Pass data through Filters
-                let mut curr_datum: Box<Option<Datum>> = Box::new(Some(datum));
-                for filter in filters.iter() {
-                    curr_datum = (filter.func)(*curr_datum);
+                let mut curr_datum: Option<Datum> = Some(datum.clone());
+                for filter in pipeline.filters.iter() {
+                    curr_datum = filter.exec(curr_datum?).unwrap();
                 }
-                curr_datum.clone()
+                curr_datum
             })
             .filter(|d| d.is_some())
             .collect()
@@ -42,19 +38,17 @@ impl Interpreter {
 
 struct Pipeline {
     data: Vec<Datum>,
-    filters: Vec<Filter>,
+    filters: Vec<Box<dyn Filter>>,
 }
 
 impl Pipeline {
-    fn new(data: Vec<Datum>, filters: Vec<Filter>) -> Self {
+    fn new(data: Vec<Datum>, filters: Vec<Box<dyn Filter>>) -> Self {
         Pipeline { data, filters }
     }
 }
 
-#[derive(Clone)]
-struct Filter {
-    name: String,
-    func: fn(Option<Datum>) -> Box<Option<Datum>>,
+pub trait Filter {
+    fn exec(&self, datum: Datum) -> Result<Option<Datum>, std::io::Error>;
 }
 
 // A single piece of data from a Source that will be fed through the Pipeline.
@@ -77,21 +71,26 @@ mod tests {
         let interpreter = Interpreter::new();
         let data_out = interpreter.exec_pipeline(pipeline);
         assert_eq!(data_out.len(), 1);
-        assert_eq!(*data_out[0], Some(Datum { data: 4 }));
+        assert_eq!(data_out[0], Some(Datum { data: 4 }));
     }
 
+    #[test]
     fn can_run_pipeline_with_filters() {
-        let double = Filter {
-            name: "double".into(),
-            func: |datum: Option<Datum>| Box::new(datum.map(|d| Datum { data: d.data * 2 })),
-        };
+        struct Double;
+        impl Filter for Double {
+            fn exec(&self, datum: Datum) -> Result<Option<Datum>, std::io::Error> {
+                Ok(Some(Datum {
+                    data: datum.data * 2,
+                }))
+            }
+        }
 
         let data = vec![Datum { data: 4 }];
-        let pipeline = Pipeline::new(data, vec![double.clone(), double]);
+        let pipeline = Pipeline::new(data, vec![Box::new(Double), Box::new(Double)]);
 
         let interpreter = Interpreter::new();
         let data_out = interpreter.exec_pipeline(pipeline);
         assert_eq!(data_out.len(), 1);
-        assert_eq!(*data_out[0], Some(Datum { data: 16 }));
+        assert_eq!(data_out[0], Some(Datum { data: 16 }));
     }
 }
